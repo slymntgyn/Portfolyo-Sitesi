9365ba021df78c1921c4e5b52a898e5254536f04
slymntgyn <49998042+slymntgyn@users.noreply.github.com>
2025-06-14 15:22:16 +0300

    hotifx

--- TuranProjects/ViewComponents/_HakkimdaComponentPartial.cs
+++ TuranProjects/ViewComponents/_HakkimdaComponentPartial.cs
@@ -2,17 +2,21 @@
 using Microsoft.AspNetCore.Mvc;
 using SERVICES.Contract;
 
-namespace TuranProjects_Portfolio.ViewComponents {
-	public class _HakkimdaComponentPartial : ViewComponent {
-		private readonly IServiceManager _manager;
+namespace TuranProjects_Portfolio.ViewComponents
+{
+    public class _HakkimdaComponentPartial : ViewComponent
+    {
+        private readonly IServiceManager _manager;
 
-		public _HakkimdaComponentPartial(IServiceManager manager) {
-			_manager = manager;
-		}
+        public _HakkimdaComponentPartial(IServiceManager manager)
+        {
+            _manager = manager;
+        }
 
-		public IViewComponentResult Invoke() {
-			Hakkinda? hakkinda = _manager.HakindaService.GetHakkindaBilgileri().FirstOrDefault();
-			return View(hakkinda);
-		}
-	}
+        public IViewComponentResult Invoke()
+        {
+            Hakkinda? hakkinda = _manager.HakindaService.GetHakkindaBilgileri().FirstOrDefault();
+            return View(hakkinda);
+        }
+    }
 }
